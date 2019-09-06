7807f1da477d8890042dd1dc9fdff5915599a2d8
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-09-06 19:24:21 +0100

    create annotated full name field on esusu group serializer
    concat admin first name and admin last name fields and annotate.
    programmatically modify querysets taken by esusu group serializer
    to contain this annotated field so that serializer can extract
    from it.
    
    NOTE: implementation collapses if serializer is ever instantiated with
    queryset via its instance argument -- which is semantically wrong
    but can still be done.
        Ex.
        Serializer(instance=Model.objects.all(), many=True)

--- tenures/serializers.py
+++ tenures/serializers.py
@@ -1,3 +1,5 @@
+from django.db.models import Value, QuerySet
+from django.db.models.functions import Concat
 from rest_framework import serializers
 
 from .models import EsusuGroup
@@ -6,13 +8,31 @@ from .models import EsusuGroup
 class EsusuGroupSerializer(serializers.HyperlinkedModelSerializer):
 
     admin_pk = serializers.ReadOnlyField(source='admin.pk')
-    admin_firstname = serializers.ReadOnlyField(source='admin.firstname')
-    admin_lastname = serializers.ReadOnlyField(source='admin.lastname')
+    admin_fullname = serializers.ReadOnlyField()
     hash_id  = serializers.ReadOnlyField()
 
     class Meta:
         model = EsusuGroup
         fields = [
-            'url', 'pk', 'name', 'hash_id',
-            'admin_pk', 'admin_firstname', 'admin_lastname'
+            'url', 'pk', 'name', 'hash_id', 'admin_pk', 'admin_fullname',
         ]
+
+    def __new__(cls, *args, **kwargs):
+        # this helps me create an annotation for fullname
+        # on the queryset operated upon by this serializer
+        if args and isinstance(args[0], QuerySet):
+            queryset = cls._build_queryset(args[0])
+            args = (queryset, ) + args[1:]
+        return super().__new__(cls, *args, **kwargs)
+
+    @classmethod
+    def _build_queryset(cls, queryset):
+        # just a helper I created
+        # not in the official interface of serializers
+        return queryset.annotate(
+            admin_fullname=Concat(
+                'admin__first_name',
+                Value(' '),
+                'admin__last_name'
+            )
+        )
